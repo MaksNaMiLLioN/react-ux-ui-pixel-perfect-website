aa6d902cc4147837d8b245e7cf7452155896b07e
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-11-23 20:41:05 +0600

    Prepare Folder Structure

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+
+import { Article, Brand, Cta, Feature, Navbar } from "./components";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
